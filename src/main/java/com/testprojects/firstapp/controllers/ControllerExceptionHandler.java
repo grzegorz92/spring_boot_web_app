d927e61199c83bd1ba583ae695ae07bdd81186f1
grzegorz92 <grzegorz.jakubiec3@gmail.com>
2018-05-04 15:55:17 +0200

    minor improvements

--- src/main/java/com/testprojects/firstapp/controllers/ControllerExceptionHandler.java
+++ src/main/java/com/testprojects/firstapp/controllers/ControllerExceptionHandler.java
@@ -2,18 +2,13 @@ package com.testprojects.firstapp.controllers;
 
 import com.testprojects.firstapp.exception.BusinessException;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.context.request.WebRequest;
-import org.springframework.web.server.MethodNotAllowedException;
-import org.springframework.web.servlet.ModelAndView;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+
 
 @ControllerAdvice
 public class ControllerExceptionHandler {
@@ -32,7 +27,7 @@ public class ControllerExceptionHandler {
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ResponseBody
     @ExceptionHandler(MissingServletRequestParameterException.class)
-    public String handleMissingServletRequestParameterException(MissingServletRequestParameterException e, WebRequest request) {
+    public String handleMissingServletRequestParameterException(MissingServletRequestParameterException e) {
 
         return "'" + e.getParameterName() + "' parameter is not present! Status Code: ";
     }
--- src/main/java/com/testprojects/firstapp/service/PropertiesServiceImpl.java
+++ src/main/java/com/testprojects/firstapp/service/PropertiesServiceImpl.java
@@ -8,7 +8,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.util.FileCopyUtils;
-import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.*;
